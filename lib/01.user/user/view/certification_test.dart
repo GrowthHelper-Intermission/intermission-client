1faae4c9ca603a2d43707fa6c327bcf503b8b2b8
puretension <rlrlfhtm5@gmail.com>
2023-11-12 18:38:29 +0900

    🎨홈스크린 UI 개선, KG 이니시스 실연동

--- lib/01.user/user/view/certification_test.dart
+++ lib/01.user/user/view/certification_test.dart
@@ -1,10 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_router/go_router.dart';
-
-/* 아임포트 휴대폰 본인인증 모듈을 불러옵니다. */
 import 'package:iamport_flutter/iamport_certification.dart';
-/* 아임포트 휴대폰 본인인증 데이터 모델을 불러옵니다. */
 import 'package:iamport_flutter/model/certification_data.dart';
 import 'package:intermission_project/01.user/user/view/certification_result.dart';
 
@@ -32,27 +30,11 @@ class CertificationTest extends StatelessWidget {
         ),
       ),
       /* [필수입력] 가맹점 식별코드 */
-      userCode: 'imp34178186',
+      userCode: '${dotenv.env['IDENTIFICATION_MID']}',
       /* [필수입력] 본인인증 데이터 */
       data: CertificationData(
-        mid: 'INIiasTest',
-        reqSvcCd: "01",
-        mTxId: "test_20210625",
-        authHash: 'ccfb95d2b12265c0b2144a2c76e30ff305cacc02e027390496d2edeab92c25d8',
-        flgFixedUser: "N",
-        userName: "홍길동",
-        userPhone: "01011112222",
-        userBirth: "19800101",
-        userHash: '',
-        directAgency: '',
-        successUrl: 'http://localhost/sasample/PHP/success.php',
-        failUrl: 'http://localhost/sasample/PHP/success.php',
         mRedirectUrl: 'http://localhost/sasample/PHP/success.php',
-        merchantUid: 'mid_${DateTime.now().millisecondsSinceEpoch}',  // 주문번호
-        company: '아임포트',                                            // 회사명 또는 URL
-        carrier: 'SKT',                                               // 통신사
-        name: '홍길동',                                                 // 이름
-        phone: '01012341234',                                         // 전화번호
+        merchantUid: 'mid_${DateTime.now().millisecondsSinceEpoch}',         // 전화번호
       ),
       /* [필수입력] 콜백 함수 */
       callback: (Map<String, String> result) {
@@ -65,7 +47,6 @@ class CertificationTest extends StatelessWidget {
                 builder: (context) => CertificationResult(result:result),
               ),
             );
-            print('wee');
           } else {
             // 본인 인증 실패 또는 취소
             Navigator.pop(context); // 이전 페이지로 돌아갑니다
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 import 'package:intermission_project/01.user/user/provider/user_me_provider.dart';
@@ -30,9 +31,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:syncfusion_flutter_core/localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart'; // 다언어 설정
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await dotenv.load(fileName: "assets/config/.env");
   runApp(
     ProviderScope(
       child: MyApp(),
@@ -54,180 +55,23 @@ class MyApp extends ConsumerWidget {
 
     return ScreenUtilInit(
       designSize: Size(375, 812),
-      builder: (context, child) =>
-          MaterialApp.router(
-            theme: ThemeData(
-              fontFamily: 'Pretendard',
-            ),
-            debugShowCheckedModeBanner: false,
-            routerConfig: route,
-            localizationsDelegates: const [ // 다언어 설정
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: [
-              const Locale('en', ''), // English
-              const Locale('ko', ''), // Korean
-            ],
-          ),
+      builder: (context, child) => MaterialApp.router(
+        theme: ThemeData(
+          fontFamily: 'Pretendard',
+        ),
+        debugShowCheckedModeBanner: false,
+        routerConfig: route,
+        localizationsDelegates: const [
+          // 다언어 설정
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('en', ''), // English
+          const Locale('ko', ''), // Korean
+        ],
+      ),
     );
   }
-
-// @override
-// Widget build(BuildContext context, WidgetRef ref) {
-//   // 라우트 로직을 여기에 추가할 수 있습니다.
-//   return ScreenUtilInit(
-//     designSize: Size(375, 812),
-//     builder: (context, child) => MaterialApp(
-//       theme: ThemeData(
-//         fontFamily: 'Pretendard',
-//       ),
-//       debugShowCheckedModeBanner: false,
-//       onGenerateRoute: (RouteSettings settings) {
-//         // 라우트 이름에 따라 다른 페이지로 라우트합니다.
-//         switch (settings.name) {
-//           case '/':
-//             return MaterialPageRoute(builder: (context) => Home()); // 예시 페이지
-//           case '/certification-result':
-//           // 인수를 받아서 다른 페이지로 전달할 수 있습니다.
-//             final result = settings.arguments as Map<String, String>;
-//             return MaterialPageRoute(builder: (context) => CertificationResult()); // 예시 페이지
-//           // default:
-//           //   return MaterialPageRoute(builder: (context) => NotFoundPage()); // 예시 페이지
-//         }
-//       },
-//       localizationsDelegates: const [  // 다언어 설정
-//         GlobalMaterialLocalizations.delegate,
-//         GlobalWidgetsLocalizations.delegate,
-//         GlobalCupertinoLocalizations.delegate,
-//       ],
-//       supportedLocales: [
-//         const Locale('en', ''), // English
-//         const Locale('ko', ''), // Korean
-//       ],
-//     ),
-//   );
-// }
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return ScreenUtilInit(
-//       designSize: Size(375, 812),
-//       builder: (context, child) => GetMaterialApp(
-//         initialRoute: '/',
-//         theme: ThemeData(
-//           fontFamily: 'Pretendard',
-//         ),
-//         debugShowCheckedModeBanner: false,
-//         getPages: [
-//           GetPage(
-//             name: '/',
-//             page: () => RootTab(),
-//           ),
-//           GetPage(name: '/splash', page: () => SplashScreen()),
-//           GetPage(
-//             name: '/test-home',
-//             page: () => Home(),
-//             middlewares: [AuthMiddleware()], // 인증 상태를 체크하는 Middleware 추가
-//           ),
-//           GetPage(name: '/certification-test', page: () => CertificationTest()),
-//           GetPage(name: '/certification', page: () => Certification()),
-//           GetPage(
-//               name: '/certification-result', page: () => CertificationResult()),
-//           GetPage(name: '/research', page: () => ResearchScreen()),
-//           GetPage(
-//             name: '/research/:id',
-//             page: () => ResearchDetailScreen(id: Get.parameters['id']!),
-//           ),
-//           GetPage(name: '/noti', page: () => NoticeScreen()),
-//           GetPage(
-//             name: '/noti/:id',
-//             page: () => NoticeDetailScreen(id: Get.parameters['id']!),
-//           ),
-//           GetPage(
-//             name: '/report',
-//             page: () => UserReportScreen(),
-//           ),
-//           GetPage(
-//             name: '/report/:id',
-//             page: () => ReportDetailScreen(id: Get.parameters['id']!),
-//           ),
-//           GetPage(name: '/signup1', page: () => SignupScreenPage1()),
-//           GetPage(name: '/signup2', page: () => SignupScreenPage2()),
-//           GetPage(name: '/signup3', page: () => SignupScreenPage3()),
-//           GetPage(name: '/select', page: () => SelectScreen()),
-//           GetPage(name: '/login', page: () => LoginScreen()),
-//           GetPage(name: '/request', page: () => ResearchReqScreen()),
-//           GetPage(name: '/notiRequest', page: () => NotiReqScreen()),
-//         ],
-//         localizationsDelegates: const [
-//           // 다언어 설정
-//           GlobalMaterialLocalizations.delegate,
-//           GlobalWidgetsLocalizations.delegate,
-//           GlobalCupertinoLocalizations.delegate,
-//         ],
-//         supportedLocales: [
-//           const Locale('en', ''), // English
-//           const Locale('ko', ''), // Korean
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class AuthController extends GetxController {
-//   var isUserAuthenticated = false.obs; // 예: 인증 상태를 저장하는 변수
-//
-//   void updateAuthStatus(bool status) {
-//     isUserAuthenticated.value = status;
-//   }
-// }
-//
-// class AuthMiddleware extends GetMiddleware {
-//   @override
-//   redirect(String? route) {
-//     var authController = Get.find<AuthController>();
-//
-//     if (route == '/signup' && authController.isUserAuthenticated.value)
-//       return RouteSettings(name: '/');
-//
-//     if (route?.startsWith('/signup') ?? false) return null;
-//
-//     if (route == '/login' && authController.isUserAuthenticated.value)
-//       return RouteSettings(name: '/');
-//
-//     // 여기에 추가적인 리다이렉션 로직을 넣을 수 있음
-//
-//     return null; // 인증되었다면 현재 라우트로 진행
-//   }
-// }
-
-// class AuthMiddleware extends GetMiddleware {
-//   @override
-//   redirect(String? route) {
-//     var authController = Get.find<AuthController>();
-//     final UserModelBase? user = ref.read(userMeProvider);
-//
-//     // 예시: 인증된 사용자만 '/certification-result'에 접근할 수 있게 함
-//     if (route == '/certification-result' && !authController.isUserAuthenticated.value) {
-//       return RouteSettings(name: '/login');  // 인증되지 않았다면 로그인 페이지로 리다이렉트
-//     }
-//
-//     // 여기에 추가적인 리다이렉션 로직을 넣을 수 있음
-//
-//     return null;  // 인증되었다면 현재 라우트로 진행
-//   }
-// }
-
-// class AuthMiddleware extends GetMiddleware {
-//   @override
-//   redirect(String? route) {
-//     var authController = Get.find<AuthController>();
-//     if (!authController.isUserAuthenticated.value) {
-//       return RouteSettings(name: '/login');  // 인증되지 않았다면 로그인 페이지로 리다이렉트
-//     }
-//     return null;  // 인증되었다면 현재 라우트로 진행
-//   }
-  }
-
+}
