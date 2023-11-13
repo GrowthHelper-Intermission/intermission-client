665e4198e66764672bb5d44b9032658f221d4153
puretension <rlrlfhtm5@gmail.com>
2023-11-14 01:37:45 +0900

    ⏰Firebase 연동, 푸쉬 알림 구현(NEW 리서치 등록시)

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,223 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:get/get.dart';
-import 'package:intermission_project/01.user/user/provider/user_me_provider.dart';
-import 'package:intermission_project/01.user/user/view/certification.dart';
-import 'package:intermission_project/01.user/user/view/certification_result.dart';
-import 'package:intermission_project/01.user/user/view/certification_test.dart';
-import 'package:intermission_project/01.user/user/view/login_screen.dart';
-import 'package:intermission_project/01.user/user/view/report_detail_screen.dart';
-import 'package:intermission_project/01.user/user/view/signup_screen_page1.dart';
-import 'package:intermission_project/01.user/user/view/signup_screen_page2.dart';
-import 'package:intermission_project/01.user/user/view/signup_screen_page3.dart';
-import 'package:intermission_project/01.user/user/view/user_report_screen.dart';
-import 'package:intermission_project/04.research/research/view/notice_detail_screen.dart';
-import 'package:intermission_project/04.research/research/view/notice_req_screen.dart';
-import 'package:intermission_project/04.research/research/view/notice_screen.dart';
-import 'package:intermission_project/04.research/research/view/research_detail_screen.dart';
-import 'package:intermission_project/04.research/research/view/research_screen.dart';
-import 'package:intermission_project/04.research/research_req/view/research_req_screen.dart';
 import 'package:intermission_project/common/provider/go_router.dart';
-import 'package:intermission_project/common/view/home.dart';
-import 'package:intermission_project/common/view/root_tab.dart';
-import 'package:intermission_project/common/view/select_screen.dart';
-import 'package:intermission_project/common/view/splash_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:syncfusion_flutter_core/localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart'; // 다언어 설정
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("백그라운드 메시지 처리중... ${message.notification!.body!}");
+}
+
+Future<String?> initializeFirebaseMessaging() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  // 메시징 서비스 기본 객체 생성
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  /// Firebase 메시징 권한 요청
+  // 첫 빌드시, 권한 확인하기
+  // 아이폰은 무조건 받아야 하고, 안드로이드는 상관 없음. 따로 유저가 설정하지 않는 한,
+  // 자동 권한 확보 상태
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print(settings.authorizationStatus);
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  /// 13버전
+  FirebaseMessaging.instance.requestPermission(
+    badge: true,
+    alert: true,
+    sound: true,
+  );
+
+  // Android용 알림 채널 설정
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.max,
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  // 로컬 알림 초기화
+  await flutterLocalNotificationsPlugin.initialize(
+    const InitializationSettings(
+      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      iOS: DarwinInitializationSettings(),
+    ),
+  );
+
+  // foreground 푸시 알림 핸들링
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+
+    print('Got a message whilst in the foreground');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification?.title,
+        notification?.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            channelDescription: channel.description,
+            icon: android.smallIcon,
+          ),
+        ),
+      );
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  // Firebase 토큰 발급
+  String? firebaseToken = await messaging.getToken();
+  print("FirebaseToken: $firebaseToken");
+
+  return firebaseToken;
+}
+
+void initializeNotification() async {
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  // 안드로이드 알림 채널 생성
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(const AndroidNotificationChannel(
+          'high_importance_channel', 'high_importance_notification',
+          importance: Importance.max));
+
+  // local alarm plugin 초기화
+  await flutterLocalNotificationsPlugin.initialize(const InitializationSettings(
+    android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+  ));
+
+  // foreground 알림 설정
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  /// 13버전
+  FirebaseMessaging.instance.requestPermission(
+    badge: true,
+    alert: true,
+    sound: true,
+  );
+}
+
+Future<String?> fcmSetting() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  await messaging.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  // foreground에서의 푸시 알림 표시를 위한 알림중요도 설정(안드로이드)
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'intermission_notification',
+    'intermission_notification',
+    description: '인터미션 알림입니다.',
+    importance: Importance.max,
+  );
+
+  // foreground 에서 푸시 알림표시를 위한 local notifications 설정
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  // foreground 푸시 알림 핸들링
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    RemoteNotification? notification = message.notification;
+    AndroidNotification? android = message.notification?.android;
+
+    print('Got a message whilst in the foreground');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null && android != null) {
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification?.title,
+        notification?.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            channelDescription: channel.description,
+            icon: android.smallIcon,
+          ),
+        ),
+      );
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  // firebase token 발급
+  String? firebaseToken = await messaging.getToken();
+
+  print("FirebaseToken : ${firebaseToken}");
+
+  return firebaseToken;
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  initializeFirebaseMessaging();
+  // String? firebaseToken = await fcmSetting();
   await dotenv.load(fileName: "assets/config/.env");
   runApp(
     ProviderScope(
@@ -47,10 +232,12 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final route = ref.watch(routeProvider);
 
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.transparent,
-      statusBarColor: Colors.transparent,
-    ));
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        systemNavigationBarColor: Colors.transparent,
+        statusBarColor: Colors.transparent,
+      ),
+    );
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
     return ScreenUtilInit(
