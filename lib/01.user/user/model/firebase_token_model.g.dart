16760b5d0efa886950e3d6c833195f030fcd05bd
puretension <rlrlfhtm5@gmail.com>
2023-11-15 02:18:03 +0900

    ⏰푸쉬알림을 위한 firebase 토큰을 서버에 전달 기능 추가

---
+++ lib/01.user/user/model/firebase_token_model.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_token_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FirebaseTokenModel _$FirebaseTokenModelFromJson(Map<String, dynamic> json) =>
+    FirebaseTokenModel(
+      token: json['token'] as String?,
+    );
+
+Map<String, dynamic> _$FirebaseTokenModelToJson(FirebaseTokenModel instance) =>
+    <String, dynamic>{
+      'token': instance.token,
+    };
