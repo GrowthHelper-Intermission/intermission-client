4e70e32a4f9af392892a291c67b52ed1a0f8bdaf
puretension <rlrlfhtm5@gmail.com>
2023-11-19 02:39:35 +0900

    🎨버전 정보 스크린 UI

--- lib/common/view/setting/version_info.dart
+++ lib/common/view/setting/version_info.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intermission_project/common/const/colors.dart';
+import 'package:intermission_project/common/view/default_layout.dart';
 
-class VersionInfo extends StatelessWidget {
-  const VersionInfo({super.key});
+class VersionInfoScreen extends StatelessWidget {
+  const VersionInfoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultLayout(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text('VersionInfo'),
+      title: '버전 정보',
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/launcher_icon/iconapp.png'),
+            Text('현재 버전은 1.1.0입니다.',style: TextStyle(fontSize: 30,color: PRIMARY_COLOR),),
+          ],
+        ),
       ),
     );
   }
