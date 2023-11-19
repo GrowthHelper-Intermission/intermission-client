e5a8728407ce33d71b9748fa2b0c9b289b9501de
puretension <rlrlfhtm5@gmail.com>
2023-11-20 02:58:59 +0900

    💵계좌 정보 변동 API 연동

---
+++ lib/01.user/user/model/change_bank_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'change_bank_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ChangeBankModel _$ChangeBankModelFromJson(Map<String, dynamic> json) =>
+    ChangeBankModel(
+      bank: json['bank'] as String?,
+      bankAccount: json['bankAccount'] as String?,
+    );
+
+Map<String, dynamic> _$ChangeBankModelToJson(ChangeBankModel instance) =>
+    <String, dynamic>{
+      'bank': instance.bank,
+      'bankAccount': instance.bankAccount,
+    };
