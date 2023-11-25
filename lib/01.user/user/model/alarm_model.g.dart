bb79ae03fb2e20a7c6195d4c907cd97d4829e507
puretension <rlrlfhtm5@gmail.com>
2023-11-26 01:57:07 +0900

    ⏰테스트 알림 기능 추가

---
+++ lib/01.user/user/model/alarm_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'alarm_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AlarmModel _$AlarmModelFromJson(Map<String, dynamic> json) => AlarmModel(
+      body: json['body'] as String,
+      title: json['title'] as String,
+    );
+
+Map<String, dynamic> _$AlarmModelToJson(AlarmModel instance) =>
+    <String, dynamic>{
+      'body': instance.body,
+      'title': instance.title,
+    };
